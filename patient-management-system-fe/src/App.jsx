f27781a592ea874440dcfc2d22d2768d21c1e6f9
MnhChag <nguyenminhtrang2905@gmail.com>
2026-03-02 11:09:10 +0700

    completed screen coordinator, dashboardReceptionist, QueueDashboad, ReceptionistProfile

--- patient-management-system-fe/src/App.jsx
+++ patient-management-system-fe/src/App.jsx
@@ -1,16 +1,35 @@
+// import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import mainRoutes from "./routes/appRoutes";
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Routes>
+//         {mainRoutes.map((r, _)=> (
+//           <Route path={r.path} element={r.element} />
+//         ))}
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App;
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import mainRoutes from "./routes/appRoutes";
+// Import ĐÚNG TÊN biến và ĐÚNG TÊN FILE bạn vừa lưu
+import receptionistRoutes from "./routes/receptionistRoutes"; 
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        {mainRoutes.map((r, _)=> (
-          <Route path={r.path} element={r.element} />
+        {receptionistRoutes.map((r, index) => (
+          // Thêm key={index} để không bị lỗi
+          <Route key={index} path={r.path} element={r.element} />
         ))}
       </Routes>
     </BrowserRouter>
-  )
+  );
 }
 
 export default App;
--- patient-management-system-fe/src/routes/receptionistRoutes.jsx
+++ patient-management-system-fe/src/routes/receptionistRoutes.jsx
@@ -1,2 +1,12 @@
-const receptionistRoutes = [];
+import Dashboard from "../pages/receptionistPage/Dashboard";
+import Coordinator from "../pages/receptionistPage/Coordinator";
+import ReceptionistProfile from "../pages/receptionistPage/receptionistProfile";
+import QueuePatientDashboard from "../pages/receptionistPage/QueuePatientDashboard";
+const receptionistRoutes = [
+    { path: '/receptionist/dashboard', element: <Dashboard /> },
+    { path: '/receptionist/coordinator', element: <Coordinator /> },
+    { path: '/receptionist/profile', element: <ReceptionistProfile /> },
+    { path: '/receptionist/queue-patient-dashboard', element: <QueuePatientDashboard /> },
+];
+
 export default receptionistRoutes;
