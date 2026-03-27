99b8cde522da5182d0b6218bdaf8d6d2503050e9
NguyenLy <nguyenly1412005@gmail.com>
2026-03-27 13:57:29 +0700

    Update LabProfilePage.jsx

--- patient-management-system-fe/src/pages/labPage/LabProfilePage.jsx
+++ patient-management-system-fe/src/pages/labPage/LabProfilePage.jsx
@@ -1,42 +1,33 @@
 import React, { useState, useEffect } from 'react';
-import { getDoctorById, updateDoctor } from '../../api/doctorApi';
-import { validateFullName, validatePhoneNumber } from '../../helpers/validationUtils';
 import ProfilePage from '../commonPage/ProfilePage';
 import toast from 'react-hot-toast';
 import { supabase } from '../../../supabaseClient';
-import { updateUser } from '../../api/userApi';
+import { getUserById, updateUser } from '../../api/userApi';
 
 
 const LabProfilePage = () => {
   const [profile, setProfile] = useState(null);
-  const [idDoctor, setIdDoctor] = useState(null);
+  const [userId, setUserId] = useState(null);
 
   const fetchProfile = async () => {
     try {
       const { data: { session } } = await supabase.auth.getSession();
 
       const idUser = session.user.id;
-      setIdDoctor(idUser);
-      const res = await getDoctorById(idUser);
+      setUserId(idUser);
+      const res = await getUserById(idUser);
       const d = res.data?.data || res.data;
 
-      setProfile((prev) => ({
-        doctor_id: d.doctor_id || '',
-        user_id: d.doctor_id || '',
-        full_name: d.Users?.full_name || '',
-        email: d.Users?.email || prev.email,
-        phone_number: d.Users?.phone_number || '',
-        specialization: d.specialization || '',
-        bio: d.bio || '',
-        avatar_url: d.Users?.avatar_url || '',
-        department_id: d.department_id || '',
-        departmentName: d.Departments.name || '',
-        room_id: d.room_id || '',
-        dob: d.Users.dob || '',
-        gender: d.Users.gender || '',
-        address: d.Users.address || '',
-        roomName: d.Rooms?.room_number || ''
-      }));
+      setProfile({
+        user_id: d.user_id || idUser,
+        full_name: d.full_name || '',
+        email: d.email || '',
+        phone_number: d.phone_number || '',
+        avatar_url: d.avatar_url || '',
+        dob: d.dob || '',
+        gender: d.gender || '',
+        address: d.address || '',
+      });
     } catch (err) {
       toast.error("Không thể tải dữ liệu")
     }
@@ -46,12 +37,12 @@ const LabProfilePage = () => {
   }, []);
 
   const handleUpdateUser = async (formData, avatarFile) => {
-    await updateDoctor(formData, avatarFile)
+    await updateUser(formData, avatarFile)
   }
   return (
     <>
       {profile && <ProfilePage
-        role="doctor"
+        role="user"
         initialData={profile}
         handleUpdateUser={handleUpdateUser}
       />}
@@ -60,3 +51,4 @@ const LabProfilePage = () => {
 };
 
 export default LabProfilePage;
+
